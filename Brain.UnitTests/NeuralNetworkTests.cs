3bd5f929f612498f0f409dc8d5884df882ce24e4
Julian Adler <julian.adler@thldigital.com>
2023-06-02 20:24:33 +1200

    done. going to bed now.

--- Brain.UnitTests/NeuralNetworkTests.cs
+++ Brain.UnitTests/NeuralNetworkTests.cs
@@ -8,35 +8,26 @@ public class NeuralNetworkTests
     [Fact]
     public void Train_ShouldTrain()
     {
-        var nn = new NeuralNetwork();
-
-        Action toTest = () => nn.Train(new TrainingDatum
-            {
-                Input = Array(0, 0),
-                Output = Array(0)
-            },
-            new TrainingDatum
-            {
-                Input = Array(0, 1),
-                Output = Array(1)
-            },
-            new TrainingDatum
-            {
-                Input = Array(1, 0),
-                Output = Array(1)
-            },
-            new TrainingDatum
-            {
-                Input = Array(1, 1),
-                Output = Array(0)
-            }
-        );
-
+        Action toTest = () => GetAndTrainXorNetwork();
         toTest.Should().NotThrow();
     }
 
     [Fact]
     public void Train_ShouldBeCorrect()
+    {
+        NeuralNetwork nn = GetAndTrainXorNetwork();
+
+        double[] result = nn.Run(new double[]
+        {
+            0,
+            1
+        });
+
+        result.Should().NotBeNull().And.HaveCount(1);
+        result[0].Should().BeGreaterThan(0.9);
+    }
+
+    private NeuralNetwork GetAndTrainXorNetwork()
     {
         var nn = new NeuralNetwork(new NeuralNetworkConfiguration
         {
@@ -75,14 +66,7 @@ public class NeuralNetworkTests
             }
         );
 
-        double[] result = nn.Run(new double[]
-        {
-            0,
-            1
-        });
-
-        result.Should().NotBeNull().And.HaveCount(1);
-        result[0].Should().BeGreaterThan(0.9);
+        return nn;
     }
 
     private static double[] Array(params double[] ints)
